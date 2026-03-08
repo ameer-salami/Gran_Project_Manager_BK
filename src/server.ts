9b960c7da5a24209852b432796cd67650dc883bb
aameer sala7i <abulameer@outlook.com>
2026-03-08 17:23:18 +0300

    cors installed and corsOptions init config set.  server.ts modified.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express, { type Express, type Request, type Response , type Application } from 'express';
 
 // const connectDB = require('./config/db');
 import connectDB from "./config/db.js"
@@ -9,10 +9,21 @@ import taskRoutes from './routes/taskRoutes.js'
 
 import dotenv from 'dotenv'
 import Logger from './util/logger.js';
+import type { CorsOptions } from 'cors';
 dotenv.config();
 
   
-  const app: Express = express();
+  const app: Application= express();
+
+  const corsOptions:CorsOptions = {
+  origin: 'http://localhost:5173', // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed custom headers
+  credentials: true, // Allow cookies to be sent with requests
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11) choke on 204
+};
+
+
   connectDB();
   app.use(express.json());
   app.use('/gran_proj_mng', workspaceRoutes)
